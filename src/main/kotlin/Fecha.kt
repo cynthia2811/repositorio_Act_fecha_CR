7cbbdbdb714b13d0620a4d0455fdef0aed3a5b26
Cynthia Rivas <cynthiarivasmoral@gmail.com>
2024-03-09 18:04:00 +0100

    refactorizacion final

--- src/main/kotlin/Fecha.kt
+++ src/main/kotlin/Fecha.kt
@@ -2,6 +2,7 @@ class Fecha(private val dia: Int, private val mes: Int, private val a: Int) {
 
 
     fun valida(): Boolean {
+        var esValida = false
 
         if (dia < 1 || dia > 31) return false
         if (mes < 1 || mes > 12) return false
